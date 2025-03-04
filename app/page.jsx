c18739be5514935c16debe769e366f29c2ab9653
ivaniraldi <iraldiban@gmail.com>
2025-03-04 09:41:04 -0300

    alert

--- app/page.jsx
+++ app/page.jsx
@@ -4,10 +4,12 @@ import RegistrationForm from "../components/RegistrationForm"
 import CountdownTimer from "../components/CountdownTimer"
 import Speakers from "../components/Speakers"
 import Footer from "../components/Footer"
+import AlertComponent from "../components/AlertComponent"
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-background">
+      <AlertComponent />
       <Hero />
       <main className="container mx-auto px-4">
         <EventDetails />
